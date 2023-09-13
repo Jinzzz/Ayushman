b97418a6a57e20d52c13dc15914101d6e5e06672
wafasalam428 <wafa.salam@hexeam.co.in>
2023-09-13 17:44:47 +0530

    sys_salary_type model created

---
+++ app/Models/Sys_Salary_Type.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sys_Salary_Type extends Model
+{
+    use HasFactory;
+    protected $table = 'sys_salary_types';
+}
