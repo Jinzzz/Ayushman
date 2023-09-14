171d2c533a7b75b1ccc56da03fe12dd239bd28d0
wafasalam428 <wafa.salam@hexeam.co.in>
2023-09-14 18:26:11 +0530

    changes style.css

--- resources/views/staffs/index.blade.php
+++ resources/views/staffs/index.blade.php
@@ -10,21 +10,21 @@
             <div class="card-body">
                 <form action="{{ route('staffs.index') }}" method="GET">
                     <div class="row mb-3">
-                        <div class="col-md-3">
+                        <div class="col-md-4">
                             <label for="staff-code">Staff Code:</label>
                             <input type="text" id="staff-code" name="staff_code" class="form-control" value="{{ request('staff_code') }}">
                         </div>
-                         <div class="col-md-3">
+                         <div class="col-md-4">
                             <label for="staff-name">Staff Name:</label>
                             <input type="text" id="staff-name" name="staff_name" class="form-control" value="{{ request('staff_name') }}">
                         </div>
-                         <div class="col-md-3">
+                         <div class="col-md-4">
                             <label for="contact-number">Contact Number:</label>
                             <input type="text" id="contact-number" name="contact_number" class="form-control" value="{{ request('contact_number') }}">
                         </div>
                    </div>
                    <div class="row mb-3">
-                        <div class="col-md-3">
+                        <div class="col-md-4">
                             <div class="form-group">
                                 <label for="staff_type">Staff Type*</label>
                                 <select class="form-control" name="staff_type" id="staff_type" onchange="toggleBookingFeeField()">
@@ -38,7 +38,7 @@
                             </div>
                         </div>
                        
-                        <div class="col-md-3">
+                        <div class="col-md-4">
                             <div class="form-group">
                                 <label for="branch_id">Branch</label>
                                 <select class="form-control" name="branch_id" id="branch_id">
@@ -51,14 +51,14 @@
                                 </select>
                             </div>
                         </div>
+                
                    
-                    
                        
-                        <div class="col-md-3 d-flex align-items-end">
-                            {{-- <div> --}}
-                                <button type="submit" class="btn btn-secondary"><i class="fa fa-filter" aria-hidden="true"></i> Filter</button>
-                                <a class="btn btn-secondary ml-2" href="{{ route('staffs.index') }}"><i class="fa fa-times" aria-hidden="true"></i> Reset</a>
-                            {{-- </div> --}}
+                        <div class="col-md-12 d-flex align-items-end">
+                           
+                                <button type="submit" class="btn btn-primary"><i class="fa fa-filter" aria-hidden="true"></i> Filter</button> &nbsp;
+                                <a class="btn btn-primary" href="{{ route('staffs.index') }}"><i class="fa fa-times" aria-hidden="true"></i> Reset</a>
+                          
                         </div>
                     </div>
                 </form>
@@ -128,7 +128,7 @@
                                         @method('PATCH')
                                             <button type="submit"
                                                 onclick="return confirm('Do you want to Change status?');"
-                                                class="btn btn-sm @if($staff->is_active == 0) btn-danger @else btn-success @endif">
+                                                class="btn btn-sm @if($staff->is_active == 0) btn-danger @else btn-success @endif status-custom">
                                                 @if($staff->is_active == 0)
                                                 InActive
                                                 @else
@@ -139,11 +139,12 @@
                                     </td>
                                        
                                     <td>
-                                        <a class="btn btn-primary btn-sm"
+                                        <a class="btn btn-primary btn-sm edit-custom"
                                             href="{{ route('staffs.edit', $staff->staff_id) }}"><i
-                                                class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit </a>
-                                                   <a class="btn btn-secondary btn-sm" href="{{ route('staffs.show', $staff->staff_id) }}">
-                                                   <i class="fa fa-eye" aria-hidden="true"></i> View    </a>
+                                                class="fa fa-pencil-square-o" aria-hidden="true" ></i> Edit </a>
+                                                   <a class="btn btn-secondary btn-sm" href="{{ route('staffs.show', $staff->staff_id) }}" style="    font-size: 0.65rem;
+                                                    margin-right: 0;">
+                                                   <i class="fa fa-eye" aria-hidden="true"></i> View</a>
 
                                         <form style="display: inline-block"
                                             action="{{ route('staffs.destroy', $staff->staff_id) }}" method="post">
@@ -154,8 +155,10 @@
                                         </form>
                                         <br>
                                         @if($staff->staff_type == '20')
-                                        <a class="btn btn-sm  btn-outline-success "
-                                        href="{{ route('staff.slot', $staff->staff_id) }}"><i
+                                        <a class="btn btn-sm  btn-primary"
+                                        href="{{ route('staff.slot', $staff->staff_id) }}" style="    font-size: 0.65rem;
+                                        margin-right: 0;
+                                        margin-top: 2px;"><i
                                             class="fa fa-pencil-square-o" aria-hidden="true"></i>Slot</a>
                                             @endif
                                     </td>
