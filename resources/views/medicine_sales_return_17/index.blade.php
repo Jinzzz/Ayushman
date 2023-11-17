e02726ed269b48249781a8e1e9239d4d307a7132
Josnathomas98 <josna.thomas@hexeam.co.in>
2023-11-17 12:27:03 +0530

    2 folder changed - sales

---
+++ resources/views/medicine_sales_return_17/index.blade.php
@@ -0,0 +1,116 @@
+@extends('layouts.app')
+@section('content')
+<div class="card">
+    @if ($message = Session::get('success'))
+    <div class="alert alert-success">
+        <p>{{$message}}</p>
+    </div>
+    @endif
+    @if ($message = Session::get('error'))
+    <div class="alert alert-danger">
+        <p>{{$message}}</p>
+    </div>
+    @endif
+    <div class="card-header">
+        <h3 class="card-title">{{$pageTitle}}</h3>
+    </div>
+    <div class="card-body">
+        <a href="{{ route('medicine.sales.return.create') }}" class="btn btn-block btn-info">
+            <i class="fa fa-plus"></i>
+            Create {{$pageTitle}}
+        </a>
+        <div class="table-responsive">
+            <table id="example" class="table table-striped table-bordered text-nowrap w-100">
+                <thead>
+                    <tr>
+                        <th class="wd-15p">SL.NO</th>
+                        <th class="wd-15p">Branch</th>
+                        <th class="wd-15p">Return No</th>
+                        <th class="wd-15p">Return Date</th>
+                        <!-- <th class="wd-15p">Total Amount</th> -->
+                        <!-- <th class="wd-15p">Discount Amount</th> -->
+                        <!-- <th class="wd-15p">Paid Amount</th> -->
+                        <th class="wd-15p">Sales person</th>
+                        <th class="wd-15p">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @php
+                    $i = 0;
+                    @endphp
+                    @foreach($purchaseReturn as $invoice)
+                    <tr id="dataRow_{{ $invoice->sales_return_id  }}">
+                        <td>{{ ++$i }}</td>
+                        <td>{{ $invoice->branch->branch_name }}</td>
+                        <td>{{ $invoice->sales_return_no }}</td>
+                        <td>{{ \Carbon\Carbon::parse($invoice->return_date)->format('d-m-Y') }}</td>
+                        <td>{{ $invoice->staff->staff_name }}</td>
+                        <!-- <td>{{ $invoice->total_amount }}</td> -->
+                        <!-- <td>{{ $invoice->discount_amount }}</td> -->
+                        <td>
+                            <a class="btn btn-primary btn-sm edit-custom" href="{{ route('medicine.sales.return.edit', $invoice->sales_return_id) }}"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit </a>
+                            <a class="btn btn-primary btn-sm edit-custom" href="{{ route('medicine.sales.return.print', $invoice->sales_return_id) }}"><i class="fa fa-print" aria-hidden="true"></i>
+                                Print </a>
+                            <a class="btn btn-secondary btn-sm" href="{{ route('medicine.sales.return.show', $invoice->sales_return_id) }}">
+                                <i class="fa fa-eye" aria-hidden="true"></i> View</a>
+                            <form style="display: inline-block" action="" method="post">
+                                @csrf
+                                @method('delete')
+                                <button type="button" onclick="deleteData({{ $invoice->sales_return_id  }})" class="btn-danger btn-sm">
+                                    <i class="fa fa-trash" aria-hidden="true"></i> Delete
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <!-- TABLE WRAPPER -->
+    </div>
+    <!-- SECTION WRAPPER -->
+</div>
+</div>
+</div>
+<!-- ROW-1 CLOSED -->
+@endsection
+<script>
+    function deleteData(dataId) {
+        swal({
+                title: "Delete selected data?",
+                text: "Are you sure you want to delete this data",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes",
+                cancelButtonText: "No",
+                closeOnConfirm: true,
+                closeOnCancel: true
+            },
+            function(isConfirm) {
+                if (isConfirm) {
+                    $.ajax({
+                        url: "{{ route('medicine.sales.return.destroy', '') }}/" + dataId,
+                        type: "DELETE",
+                        data: {
+                            _token: "{{ csrf_token() }}",
+                        },
+                        success: function(response) {
+                            // Handle the success response, e.g., remove the row from the table
+                            if (response == '1') {
+                                $("#dataRow_" + dataId).remove();
+                                flashMessage('s', 'Data deleted successfully');
+                            } else {
+                                flashMessage('e', 'An error occured! Please try again later.');
+                            }
+                        },
+                        error: function() {
+                            alert('An error occurred while deleting the data.');
+                        },
+                    });
+                } else {
+                    return;
+                }
+            });
+    }
+</script>
---
+++ resources/views/medicine_sales_return_17/print_invoice.blade.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Ayushman Ayurveda | Invoice</title>
+</head>
+
+<body style="box-sizing: border-box; margin: 0; font-family: Arial, Helvetica, sans-serif;">
+
+    <div style="max-width: 600px; margin: auto;">
+        <table style="width: 100%; border: none; border-top: 5px solid #3DAA33;">
+            <colgroup>
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+            </colgroup>
+            <tr style="vertical-align: bottom; border-bottom: 1px solid #000;">
+                <th colspan="5" style="text-align: left; border: none; padding: 15px 0;">
+                    <img src="https://gcdnb.pbrd.co/images/UzY2RLjm3Sb1.png?o=1" style="max-width: 200px;">
+                </th>
+                <th colspan="5" style="text-align: right; border: none; padding: 15px 0;">
+                    <h2 style="margin: 0 0 13px;">Invoice</h2>
+                </th>
+            </tr>
+            <tr>
+                <td colspan="10" style="border-top: 1px solid #ddd;"></td>
+            </tr>
+            <tr>
+                <td colspan="5" style="border: none; padding: 15px 0 0;">
+                    <p style="margin: 0;">Return Invoice No.:
+                        <b>{{$data['sales_invoice_number']}}</b>
+                    </p>
+                </td>
+                <td colspan="5" style="border: none; text-align: right; padding: 15px 0 0;">
+                    <p style="margin: 0;">Date:
+                        <b>{{ date('d-m-Y', strtotime($data['invoice_date'])) }}</b>
+                    </p>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="5" style="border: none; padding: 15px 0;">
+                    <p style="margin: 0;">Patient Name:
+                        <b>{{$data['patient_name']}}</b>
+                    </p>
+                </td>
+                <td colspan="5" style="border: none; text-align: right; padding: 15px 0;">
+                    <p style="margin: 0;">Patient Code:
+                        <b>{{$data['patient_code']}}</b>
+                    </p>
+                </td>
+            </tr>
+        </table>
+
+        <table style="width: 100%; border: 1px solid #999; border-collapse: collapse; margin: 10px 0 0;">
+            <colgroup>
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+            </colgroup>
+            <tr style="text-align: left;">
+                <th colspan="6" style="padding: 10px; border: 1px solid #999; background: #f1f2f2;">Medicine Name</th>
+                <th colspan="2" style="padding: 10px; border: 1px solid #999; background: #f1f2f2;">Qty</th>
+                <th colspan="2" style="padding: 10px; border: 1px solid #999; background: #f1f2f2;">Amt</th>
+            </tr>
+            @foreach($medicine_sale_details as $invoice)
+            <tr>
+                <td colspan="6" style="padding: 10px; border: 1px solid #999;">{{$invoice->medicine->medicine_name}}</td>
+                <td colspan="2" style="padding: 10px; border: 1px solid #999;">{{intval($invoice->quantity)}}</td>
+                <td colspan="2" style="padding: 10px; border: 1px solid #999;">{{ number_format($invoice->amount, 2) }}</td>
+            </tr>
+            @endforeach
+        </table>
+
+        <table style="width: 100%; border: none;">
+            <colgroup>
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+                <col width="10%">
+            </colgroup>
+            <tr>
+                <td colspan="10" style="border: none; padding: 10px 0 0; text-align: right;">
+                    <p style="margin: 15px 0 0;">Subtotal : {{ number_format($data['sub_total'], 2) }}</p>
+                    <p style="margin: 15px 0 0;">Tax Amount : {{ number_format($data['tax_amount'], 2) }}</p>
+                    <h3 style="margin: 15px 0 0;"><b>Total Amount : {{ number_format($data['total_amount'], 2) }}</b></h3>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="10" style="border: none; padding: 10px 0 0; text-align: center;">
+                    <p style="padding: 15px 0; background: #004f27; color: #fff;">Thank you for choosing our services. Your satisfaction is our priority.</p>
+                </td>
+            </tr>
+        </table>
+
+    </div>
+
+</body>
+
+</html>
